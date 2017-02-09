e89b05a1d1cc82dab34259b95d2c6ceb5389435b
Tom Harman <me@MacBook-Pro-2.local>
2017-02-09 12:28:11 -0500

    Added background color randomization

--- order/main.js
+++ order/main.js
@@ -1,6 +1,35 @@
 // Setup
 
 var names = ['Cait','Allison','Lindsey','Wolasi','Emily'];
+var colors = [
+'#EE3322',
+'#BA1B16',
+'#910A0A',
+'#FFEE00',
+'#C1A000',
+'#997200',
+'#F43192',
+'#C41475',
+'#870753',
+'#6645DD',
+'#41259D',
+'#301984',
+'#0F65EF',
+'#0030BA',
+'#00298E',
+'#0DCCB0',
+'#007C7C',
+'#005B60',
+'#68AF15',
+'#3C7A05',
+'#2D5B00',
+'#F7AD19',
+'#CC8200',
+'#A56200',
+'#F47F16',
+'#BF5608',
+'#964108'
+];
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -31,6 +60,7 @@ function init() {
 	list = shuffle(names);
 
   var html = "<ol>";
+  var randomBg = getRandomInt(0,colors.length);
   var jokester = getRandomInt(0,4);
 
 	for (var a = 0; a < names.length; a++) {
@@ -42,7 +72,11 @@ function init() {
 	}
 	html += "</ol>";
 
+  // change bg color
+  html += "<style type=\"text/css\">body{ background: "+colors[randomBg]+" !important; }</style>";
+
 	// output full list
 	d = document.getElementById("results");
 	d.innerHTML = html;
+
 }
